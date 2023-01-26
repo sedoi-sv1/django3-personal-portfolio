4f945cf574c0cee0198837a29c1c6ee2614ea305
Sedoi <sedoi.sv1@yandex.ru>
2023-01-27 03:00:54 +0700

    First commit!

---
+++ portfolio/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Project
+
+def home(request):
+    projects = Project.objects.all()
+    return render(request, 'portfolio/home.html', {'projects':projects})
+
+def all_blogs(request):
+	return render(request, 'blog/all_blogs.html') 
